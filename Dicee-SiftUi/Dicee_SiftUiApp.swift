48986e957d2de4ff907ad3cb6060511e9f5f276f
Shreyash <shreyashp47@gmail.com>
2024-01-02 22:16:17 +0530

    Initial Commit

---
+++ Dicee-SiftUi/Dicee_SiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SiftUiApp.swift
+//  Dicee-SiftUi
+//
+//  Created by Shreyash Pattewar on 02/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
